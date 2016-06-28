b286054e543cd28f519ef73b3bdc12b3d99e4c45
zdyGit <1193009984@qq.com>
2016-06-28 21:31:03 +0800

    AlgotithmSet

---
+++ AlgorithmSet.py
@@ -0,0 +1,41 @@
+import math
+
+#得到两者相似项
+def GetSameItem(UL,p1,p2):
+	si = {}
+	for item in UL[p1]:
+		if item in UL[p2]:
+			si[item] = 1
+	return si
+
+#欧几里得相似度算法
+def EuclidSimilarity(UL,p1,p2):
+	si = GetSameItem(UL,p1,p2)
+	if len(si) == 0:
+		return 0
+	sum_of_squares = sum([pow(UL[p1][item] - UL[p2][item] , 2) for item in si])
+	return 1/(1+math.sqrt(sum_of_squares))
+
+#皮尔逊相似度算法
+def PearsonSimilarity(UL,p1,p2):
+	si = GetSameItem(UL,p1,p2)
+	n = len(si)
+	if n == 0:
+		return 0
+
+	sum1 = sum([UL[p1][item] for item in si])
+	sum2 = sum([UL[p2][item] for item in si])
+
+	sqSum1 = sum([pow(UL[p1][item],2) for item in si])
+	sqSum2 = sum([pow(UL[p2][item],2) for item in si])
+
+	pSum = sum([UL[p1][item]*UL[p2][item] for item in si])
+
+	num = pSum - (sum1*sum2/n)
+	den = math.sqrt(sqSum1-pow(sum1,2)/n)*math.sqrt(sqSum2-pow(sum2,2)/n)
+
+	if den ==0:
+		return 0
+
+	r = num/den
+	return r
--- Start.py
+++ Start.py
@@ -1,28 +1,101 @@
 import FileTool
+import AlgorithmSet
 
 userInfoPath = "\\MoviesDataSet\\u.user"
+moviesInfoPath = "\\MoviesDataSet\\u.item"
+scoreInfoPath = "\\MoviesDataSet\\u.data"
 
-
-
+#获取同事信息
 def GetUserData():
-
 	userLines = FileTool.GetAllDataInline(userInfoPath)
 	userList = {}
 	for line in userLines:
 		userInfoArr = line.strip('\n').split('|')
 		user = {}
-		user["userID"] = userInfoArr[0]
-		user["userAge"] = userInfoArr[1]
+		user["userID"] = int(userInfoArr[0])
+		user["userAge"] = int(userInfoArr[1])
 		user["userSex"] = userInfoArr[2]
 		user["userProfession"] = userInfoArr[3]
 		if user["userID"] not in userList.keys():
 			userList[user["userID"]] = user
 	return userList
 
-def LoadUserInfo():
-	print("Load User Data ……")
-	ul = GetUserData()
-	print("Load User Data Finished !")
-	return ul
+#获取影片信息
+def GetMovieData():
+	movieLines = FileTool.GetAllDataInline(moviesInfoPath)
+	movieList = {}
+	for line in movieLines:
+		movieInfoArr = line.strip('\n').split('|')
+		movie = {}
+		movie["movieID"] = int(movieInfoArr[0])
+		movie["title"] = movieInfoArr[1]
+		movie["release date"] = movieInfoArr[2]
+		movie["video release date"] = movieInfoArr[2]
+		movie["IMDb URL"] = movieInfoArr[4]
+		movie["unknown"] = int(movieInfoArr[5])
+		movie["Action"] = int(movieInfoArr[6])
+		movie["Adventure"] = int(movieInfoArr[7])
+		movie["Animation"] = int(movieInfoArr[8])
+		movie["Children's"] = int(movieInfoArr[9])
+		movie["Comedy"] = int(movieInfoArr[10])
+		movie["Crime"] = int(movieInfoArr[11])
+		movie["Documentary"] = int(movieInfoArr[12])
+		movie["Drama"] = int(movieInfoArr[13])
+		movie["Fantasy"] = int(movieInfoArr[14])
+		movie["Film-Noir"] = int(movieInfoArr[15])
+		movie["Horror"] = int(movieInfoArr[16])
+		movie["Musical"] = int(movieInfoArr[17])
+		movie["Mystery"] = int(movieInfoArr[18])
+		movie["Romance"] = int(movieInfoArr[19])
+		movie["Sci-Fi"] = int(movieInfoArr[20])
+		movie["Thriller"] = int(movieInfoArr[21])
+		movie["War"] = int(movieInfoArr[22])
+		movie["Western"] = int(movieInfoArr[23])
+		if movie["movieID"] not in movieList.keys():
+			movieList[movie["movieID"]] = movie
+	return movieList
 
-LoadUserInfo()
+#获取评分记录
+def GetScoreData():
+	scoreLines = FileTool.GetAllDataInline(scoreInfoPath)
+	scoreList = {}
+	for line in scoreLines:
+		scoreInfoArr = line.strip('\n').split()
+		userID = int(scoreInfoArr[0])
+		movieID = int(scoreInfoArr[1])
+		score = int(scoreInfoArr[2])
+		if userID not in scoreList:
+			scoreList.setdefault(userID,{})
+			scoreList[userID].setdefault(movieID,score)
+		else:
+			scoreList[userID].setdefault(movieID,score)
+	return scoreList
+
+
+#计算相似度
+def GetSimilarityResult(UL,p,n,similarMethod):
+	res = {}
+	for other in UL:
+		if other == p:
+			continue
+		else:
+			res.setdefault(other,similarMethod(UL,p,other))
+	return res
+
+
+def Main():
+
+	ul = GetScoreData()
+	p1 = 149
+	p2 = 122
+	
+	si  = AlgorithmSet.GetSameItem(ul,p1,p2)
+	for item in si:
+		print("p1-"+str(item)+":"+str(ul[p1][item]))
+		print("p2-"+str(item)+":"+str(ul[p2][item]))
+
+	print(AlgorithmSet.PearsonSimilarity(ul,p1,p2))
+	print(AlgorithmSet.EuclidSimilarity(ul,p1,p2))
+	#print(GetSimilarityResult(ul,121,10,AlgorithmSet.EuclidSimilarity))
+
+Main()
